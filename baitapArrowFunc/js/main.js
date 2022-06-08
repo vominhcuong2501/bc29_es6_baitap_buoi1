02606358dce5e8b061bb0b96c226f3ead95a6053
vominhcuong2501 <minhcuongvo2501@gmail.com>
2022-06-08 18:43:04 +0700

    update

--- baitapArrowFunc/js/main.js
+++ baitapArrowFunc/js/main.js
@@ -5,52 +5,41 @@ let colorList = ["pallet", "viridian", "pewter", "cerulean", "vermillion", "lave
 /**
  * Tạo nút button màu
  */
-const createButton = () => {
-    let content = ""; 
-    for(let i = 0; i < colorList.length; i++) {  
-        content +=`<button class = "color-button ${colorList[i]}"
-        id= "${colorList[i]}" onclick = "${colorList[i]}()"></button>`;
-    }
-    getEle("colorContainer").innerHTML = content;
-}
-createButton();
+// const createButton = () => {
+//     let content = ""; 
+//     for(let i = 0; i < colorList.length; i++) {  
+//         content +=`<button class = "color-button ${colorList[i]}"
+//         id= "${colorList[i]}" onclick = "${colorList[i]}()"></button>`;
+//     }
+//     getEle("colorContainer").innerHTML = content;
+// }
+// createButton();
 
-for(let i = 0; i < colorList.length; i++) { 
-    getEle(colorList[i]).onclick = () => {
-        getEle("house").classList.add(colorList[i]);
-        
-    }
-}
+  colorList.forEach((currentValue, currentIndex, currentArray) => {
+    let btn = document.createElement("button");
+    btn.className = `color-button ${currentValue}`;
+    getEle("colorContainer").appendChild(btn);
+  });
+  
+  let btnContainer = getEle("colorContainer");
+  console.log(btnContainer);
+  let btns = btnContainer.getElementsByClassName("color-button");
+  console.log(btns);
 
-
-// const pallet = () => {
-//     getEle("house").classList.add('pallet');
-// }
-// const viridian = () => {
-//     getEle("house").classList.add('viridian');
-// }
-// const pewter = () => {
-//     getEle("house").classList.add('pewter');
-// }
-// const cerulean = () => {
-//     getEle("house").classList.add('cerulean');
-// }
-// const vermillion = () => {
-//     getEle("house").classList.add('vermillion');
-// }
-// const lavender = () => {
-//     getEle("house").classList.add('lavender');
-// }
-// const celadon = () => {
-//     getEle("house").classList.add('celadon');
-// }
-// const saffron = () => {
-//     getEle("house").classList.add('saffron');
-// }
-// const fuschia = () => {
-//     getEle("house").classList.add('fuschia');
-// }
-// const cinnabar = () => {
-//     getEle("house").classList.add('cinnabar');
-// }
-
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      let current = document.getElementsByClassName("active");
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" active", "");
+      }
+      this.className += " active";
+  
+      colorList.forEach((ele, idx) => {
+        let currentHouse = getEle("house").classList;
+        if (currentHouse.contains(ele)) {
+          currentHouse.remove(ele);
+        }
+        currentHouse.add(this.classList[1]);
+      });
+    });
+  }
